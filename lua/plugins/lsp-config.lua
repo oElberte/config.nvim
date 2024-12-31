808291f83fb2e70263c4098b7ffcdd4c9c34fa42
Elberte Plínio <dev@elberte.com>
2024-12-31 12:46:59 -0300

    feat: adds `Autocomplete` and `Snippets`

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,32 +1,43 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    lazy=false,
-    config = function()
-      require("mason").setup()
-    end
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "ast_grep", "svelte", "tailwindcss" }
-      })
-    end
-  },
-  {
-    "neovim/nvim-lspconfig",
-    lazy=false,
-    config = function()
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.ast_grep.setup({})
-      lspconfig.svelte.setup({})
-      lspconfig.tailwindcss.setup({})
+	{
+		"williamboman/mason.nvim",
+		lazy = false,
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "ast_grep", "svelte", "tailwindcss" },
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-      vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
-    end
-  }
+			local lspconfig = require("lspconfig")
+
+			lspconfig.ast_grep.setup({
+				capabilities = capabilities
+			})
+			lspconfig.svelte.setup({
+				capabilities = capabilities
+			})
+			lspconfig.tailwindcss.setup({
+				capabilities = capabilities
+			})
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities
+			})
+
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
