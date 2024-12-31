64bfa3f7e15863e9e1806809bfaf198fc9e83e9a
Elberte Plínio <dev@elberte.com>
2024-12-31 08:14:25 -0300

    first commit

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,32 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    lazy=false,
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "ast_grep", "svelte", "tailwindcss" }
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy=false,
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({})
+      lspconfig.ast_grep.setup({})
+      lspconfig.svelte.setup({})
+      lspconfig.tailwindcss.setup({})
+
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+      vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
+    end
+  }
+}
